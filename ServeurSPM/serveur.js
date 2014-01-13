3360db05fca069931110c77bd40845adef06e212
lchuiton <lchuiton@gmail.com>
2014-01-13 17:07:24 +0100

    modification sur le serveur node.js

--- ServeurSPM/serveur.js
+++ ServeurSPM/serveur.js
@@ -3,23 +3,68 @@ var express = require('express');
 var app = express();
 
 var backlog = [
-               {numeroUS:1, contenuUS:'ETQ Dev, je veux des US dans mon gui'},
-               {numeroUS:2, contenuUS:'ETQ Dev, je veux faire des tests sur NodeJS'}
+               {numeroUS:1, contenuUS:'ETQ Dev, je veux des US dans mon gui', numeroPriorisation:1},
+               {numeroUS:2, contenuUS:'ETQ Dev, je veux faire des tests sur NodeJS', numeroPriorisation:2}
                ];
- 
+ // Retourne un JSON qui contient les US du Backlog priorisées
 app.get('/backlog', function(req, res) {
-    res.json(backlog);
+	arr = trierTableauParPriorite(backlog);
+	res.json(arr);
 });
 
+// Modifie la priorité d'une US et "décale" la priorisation des autres US si besoin
+app.get('/backlog/:id/:idUsPrecedente', function() {
+	rechercheSiUSExiste(req.params.id);
+
+	var position = getUS(req.params.id).numeroPriorisation;
+
+});
+
+// Affiche une US en particulier
 app.get('/backlog/:id', function(req, res) {
-	if(backlog.length <= req.params.id || req.params.id < 0) {
+	rechercheSiUSExiste(req.params.id) ;
+	
+	var us = getUS(req.params.id);
+
+	if(us != null) {
+		res.json(us);
+	} else {
+		res.statusCode = 404;
+	    return res.send('Error 404: No story found in backlog');
+	}
+});
+
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');
+
+var getUS = function(numUS) {
+	var indexDuBacklog = 0;
+
+	while (indexDuBacklog < backlog.length) {
+
+		if(backlog[indexDuBacklog].numeroUS ==numUS) {
+
+			var us = backlog[indexDuBacklog];
+			return us;
+ 		}
+		indexDuBacklog++;
+	}
+
+
+}
+
+var trierTableauParPriorite = function(tableau){
+		return tableau.sort(function(a,b) {
+ 		a.numeroPriorisation > b.numeroPriorisation;
+ 		});
+
+
+};
+
+var rechercheSiUSExiste = function(id) {
+	if(id < 0) {
 	    res.statusCode = 404;
 	    return res.send('Error 404: No story found in backlog');
 	}
-	
-	var us = backlog[req.params.id];
-    res.json(us);
-});
- 
-app.listen(3000);
-console.log('Listeninger on port 3000...');
+}
